8bf0e570d75f3d9719261f0287e4331e50909859
Manvitha <manvithachinni444@gmail.com>
2025-08-18 08:14:03 +0530

    initial commit

---
+++ TicTacTeo/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  TicTacTeo
+//
+//  Created by Manvitha on 18/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var turnLabel: UILabel!
+
+    @IBOutlet weak var a1Button: UIButton!
+    @IBOutlet weak var a2Button: UIButton!
+    @IBOutlet weak var a3Button: UIButton!
+    @IBOutlet weak var b1Button: UIButton!
+    @IBOutlet weak var b2Button: UIButton!
+    @IBOutlet weak var b3Button: UIButton!
+    @IBOutlet weak var c1Button: UIButton!
+    @IBOutlet weak var c2Button: UIButton!
+    @IBOutlet weak var c3Button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func SelectedButton(_ sender: UIButton) {
+    }
+    
+
+
+}
+
---
+++ TicTacTeoTests/TicTacTeoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacTeoTests.swift
+//  TicTacTeoTests
+//
+//  Created by Manvitha on 18/08/25.
+//
+
+import Testing
+@testable import TicTacTeo
+
+struct TicTacTeoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
